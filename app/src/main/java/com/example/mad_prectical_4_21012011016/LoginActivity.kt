ef11369a124308e59304025abfb9a5e81d51fc3a
Krish <darjikrish1012@gmail.com>
2023-11-02 11:09:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_prectical_4_21012011016/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_prectical_4_21012011016
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
